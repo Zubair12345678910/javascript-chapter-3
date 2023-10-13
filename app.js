8dc7aefc900936efcbace9a04bb2b1097d3a6267
Zubair12345678910 <zubairsheihh67@gmail.com>
2023-10-13 09:47:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var age = "i am 18 years old "
+
+alert(age)
+
+var information = "you have visited this site 25 times"
+
+alert(information)
+
+var doc1 = "my age is 24 "
+
+var doc2 = "data type of my decleared variable is number"
+
+document.write(doc1+"<br/>"+doc2)
+
+// document.write("<h1>jhone Doe<h1/>")
+
+
+document.write("<h1>jhoe deo ordered t-shirt(s)on xyz clothing store </h1>")
+
+
+// var visitorsname = "<h1>jhoneDoe<h1/>"
+// var producttitle = "<h1>t-shirt<h1/>"
+
+
